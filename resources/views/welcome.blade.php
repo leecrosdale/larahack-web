1b5edbadf103638fae076114c587f55e2a17f754
Lee Crosdale <accounts@leecrosdale.com>
2018-01-21 09:36:21 +0000

    DigitalOcean*

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -129,9 +129,9 @@
                     <div class="section-title text-center">
                         <h3 class="text-white">Prizes</h3>
                         <span class="text-white text-uppercase">
-                            <h3 class="text-white"> First Place: $500 in Digital Ocean Credits</h3>
-                            <h3 class="text-white"> Second Place: $250 in Digital Ocean Credits</h3>
-                            <h3 class="text-white">Third Place: $100 in Digital Ocean Credits</h3>
+                            <h3 class="text-white"> First Place: $500 in DigitalOcean Credits</h3>
+                            <h3 class="text-white"> Second Place: $250 in DigitalOcean Credits</h3>
+                            <h3 class="text-white">Third Place: $100 in DigitalOcean Credits</h3>
                         </span>
                     </div>
                 </div>
