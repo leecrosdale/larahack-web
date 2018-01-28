a7414c74a2fe88142d9772d2f33c5e2254567343
Lee Crosdale <accounts@leecrosdale.com>
2018-01-28 11:29:02 +0000

    Renamed header of request page

--- resources/views/join/index.blade.php
+++ resources/views/join/index.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Posts - <a href="{{ url('posts/create') }}">Create a Post</a></div>
+                    <div class="panel-heading">Requests to join Projects</div>
 
                     <div class="panel-body">
                         @if (session('status'))
