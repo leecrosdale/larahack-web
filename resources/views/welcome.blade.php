16a02ae7efdc283a5e1c6335c24718d1394d8ca9
Lee Crosdale <accounts@leecrosdale.com>
2018-02-02 21:00:18 +0000

    Fixed issue with logo href loading index.html

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,7 +60,7 @@
             <div class="header-wrap">
                 <div class="header-top d-flex justify-content-between align-items-center">
                     <div class="logo">
-                        <a href="index.html"><img src="img/logo.png" alt=""></a>
+                        <a href="{{ url('') }}"><img src="img/logo.png" alt=""></a>
                     </div>
                     <div class="main-menubar d-flex align-items-center">
                         <nav class="hide">
