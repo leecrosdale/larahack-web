d6467b5ebb3c79450c833559ddb76e545e124fab
Lee Crosdale <accounts@leecrosdale.com>
2018-04-13 15:15:01 +0100

    Show old projects, prevent new projects from being created before the event starts

--- app/Project.php
+++ app/Project.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    protected $fillable = ['title','description','url','repo', 'image'];
+    protected $fillable = ['title','description','url','repo', 'image','event_id'];
 
     public function users() {
         return $this->belongsToMany('App\User');
