fa30fba6fa060cf762a243300e0f2c1d3a2253a1
Lee Crosdale <accounts@leecrosdale.com>
2018-06-06 09:10:41 +0100

    only active events

--- app/Helpers/Stage.php
+++ app/Helpers/Stage.php
@@ -43,7 +43,7 @@ class Stage
         $event = Event::where('active',1)->where('event_start', '>=', Carbon::now()->toDateTimeString())->where('event_end', '<=', Carbon::now()->toDateTimeString())->first();
 
         if (!$event) {
-            $event = Event::orderBy('event_start', 'DESC')->where('event_end', '>=', Carbon::now()->toDateTimeString())->first();
+            $event = Event::where('active',1)->orderBy('event_start', 'DESC')->where('event_end', '>=', Carbon::now()->toDateTimeString())->first();
         }
 
         return $event;
