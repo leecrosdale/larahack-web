cdb4dc8be814a62651af4241e0a6d6199bec0f3e
Lee Crosdale <accounts@leecrosdale.com>
2019-02-22 21:32:43 +0000

    Linode Credit

--- resources/views/swag/index.blade.php
+++ resources/views/swag/index.blade.php
@@ -20,7 +20,7 @@
 
                                <h2>Here's $15 Linode credit!</h2><br/>
                                 For their smallest 1GB plan, that’s 3 months of service.<br/>
-                                Once the event is running, Just head to Linode.com and enter the promo code, <b>LARAHACK2</b>
+                                Once the event is running, Just head to Linode.com and enter the promo code, <b>LARAHACK3</b>
                         @else
                                 <h2>Swag will appear here once the event starts.</h2>
                         @endif
