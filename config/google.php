4edf7e10702adc3fb9d22b78fc1f1bb938c9ba93
Lee Crosdale <accounts@leecrosdale.com>
2020-10-13 14:23:13 +0100

    Holding page. Thanks everyone <3

--- config/google.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'recaptcha_key' => env('GOOGLE_RECAPTCHA_KEY'),
-    'recaptcha_secret' => env('GOOGLE_RECAPTCHA_SECRET'),
-    'recaptcha_score' => env('GOOGLE_RECAPTCHA_SCORE')
-];
--- public/index.php
+++ public/index.php
@@ -1,60 +1,90 @@
-<?php
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+<head>
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+    <meta name="google-site-verification" content="8hXPsBeIeEwKfMyoLpMuzjy9GvWlYxD8hMqDtWyIXXM"/>
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-define('LARAVEL_START', microtime(true));
+    <!-- Author Meta -->
+    <meta name="author" content="CodePixar">
+    <!-- Meta Description -->
+    <meta name="description" content="LaraHack - A Laravel Hackathon">
+    <!-- Meta Keyword -->
+    <meta name="keywords" content="Laravel Hackathon LaravelUK Every 3 months">
+    <!-- meta character set -->
+    <meta charset="UTF-8">
+    <!-- Site Title -->
+    <title>LaraHack - The Laravel Hackathon</title>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
-*/
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,500,600" rel="stylesheet">
+    <!--
+    CSS
+    ============================================= -->
+    <link rel="stylesheet" href="css/linearicons.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/nice-select.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
 
-require __DIR__.'/../vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
+</head>
+<body>
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+<div class="main-wrapper-first" id="app">
+    <div class="banner-area">
+        <div class="container">
+            <div class="row justify-content-center height align-items-center">
+                <div class="col-lg-8">
+                    <div class="banner-content text-center">
+                        <span class="text-white top text-uppercase">LaraHack - The Online Laravel Hackathon</span>
+						
+						<h3 class="text-white">The LaraHack event is postponed indefinitely, I'd like to thank all that took part during the many events, I'd love to get involved with a similar project/event in the future.<br/><br/>Lee C.</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="subscription-area">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="section-title text-center">
+                        <h3>Contact Us</h3>
+                        <span class="text-uppercase">Have a question? Contact us here.</span>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <div id="mc_embed_signup">
+                        <i> Email: <a href="mailto:lee@larahack.com">lee@larahack.com</a> | <a
+                                    href="https://crosdale.dev" target="_blank">crosdale.dev</a></i>
+                    </div>
+                </div>
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+            </div>
+        </div>
+    </section>
+    <!-- End Subscription Area -->
+    <!-- Start footer Area -->
+    <footer>
+        <div class="container">
+            <div class="footer-content d-flex justify-content-between align-items-center flex-wrap">
+                <div class="logo">
+                    <img src="img/logo.png" alt="">
+                </div>
+                <div class="footer-social">
+                    <a href="https://twitter.com/leecrosdale"><i class="fa fa-twitter"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End footer Area -->
+</div>
+</body>
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+</body>
+</html>
